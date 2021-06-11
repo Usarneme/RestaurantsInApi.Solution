c33da5dd475dc00b3b5a258c304113f9fa1c0843
Usarneme <jamestcoop@gmail.com>
2021-06-11 14:11:43 -0700

    :building_construction: adds architecture for JWT token-based auth for api routes

---
+++ RestaurantsInApi/Helpers/AppSettings.cs
@@ -0,0 +1,7 @@
+namespace RestaurantsInApi.Helpers
+{
+  public class AppSettings
+  {
+    public string Secret { get; set; }
+  }
+}
--- RestaurantsInApi/Startup.cs
+++ RestaurantsInApi/Startup.cs
@@ -18,18 +18,22 @@ namespace RestaurantsInApi
 {
   public class Startup
   {
+    public IConfiguration Configuration { get; }
     public Startup(IConfiguration configuration)
     {
       Configuration = configuration;
     }
 
-    public IConfiguration Configuration { get; }
-
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
+      services.AddCors();
       services.AddDbContext<RestaurantsInApiContext>(opt => opt.UseInMemoryDatabase("RestaurantsInApi"));
       services.AddControllers();
+      // configure strongly typed settings object
+      services.Configure<AppSettings>(Configuration.GetSection("AppSettings"));
+      // configure dependency injection for application services
+      services.AddScoped<IUserService, UserService>();
       services.AddSwaggerGen(c =>
       {
         c.SwaggerDoc("v1", new OpenApiInfo { Title = "RestaurantsInApi", Version = "v1" });
@@ -42,16 +46,19 @@ namespace RestaurantsInApi
       if (env.IsDevelopment())
       {
         app.UseDeveloperExceptionPage();
-        app.UseSwagger();
-        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "RestaurantsInApi v1"));
       }
-
-      app.UseHttpsRedirection();
-
+      app.UseSwagger();
+      app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "RestaurantsInApi v1"));
+      // app.UseHttpsRedirection();
       app.UseRouting();
-
       app.UseAuthorization();
-
+      // global CORS policy
+      app.UseCors(x => x
+        .AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader());
+      // custom jwt auth middleware
+      app.UseMiddleware<JwtMiddleware>();
       app.UseEndpoints(endpoints =>
       {
         endpoints.MapControllers();
